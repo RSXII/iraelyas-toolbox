900e350c2369c2581ef64e659210e516a2ba6974
Ryan Schultz <ryanschultz1985@gmail.com>
2026-06-17 19:33:43 -0400

    feat(favor): enhance favor player block layout with portrait and insignia support

--- src/features/favor/FavorNpcList.svelte
+++ src/features/favor/FavorNpcList.svelte
@@ -71,6 +71,9 @@
       {@const renownMeta  = favorMeta(renownScore, tiers)}
 
       <div class="section-head">
+        {#if fc.insignia}
+          <img class="favor-section-insignia" src={fc.insignia} alt="" aria-hidden="true" />
+        {/if}
         <span class="section-name">{fc.name}</span>
         {#if editEnabled}
           <div class="faction-reorder-btns">
@@ -88,12 +91,18 @@
       <!-- Renown row -->
       <div class="npc-row faction-header">
         <div class="npc-left">
-          <div style="width:28px"></div>
+          {#if editEnabled}<div style="width:28px"></div>{/if}
           <div class="reorder-btns" style="visibility:hidden">
             <button class="reorder-arrow" disabled>▲</button>
             <button class="reorder-arrow" disabled>▼</button>
           </div>
-          <div class="npc-initials" style="font-size:10px">{fc.name.slice(0,2).toUpperCase()}</div>
+          <div class="npc-initials" class:npc-favor-portrait={!!fc.insignia} style="font-size:10px">
+              {#if fc.insignia}
+                <img src={fc.insignia} alt="" aria-hidden="true" />
+              {:else}
+                {fc.name.slice(0,2).toUpperCase()}
+              {/if}
+            </div>
           <div class="npc-info">
             <div class="npc-name">{fc.name} <span class="faction-header-badge">Renown</span></div>
             <div class="npc-role">Faction</div>
